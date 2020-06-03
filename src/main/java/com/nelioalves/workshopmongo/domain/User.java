5b77cba6e653e5a595bed54121db3189c66f5fac
Michael Aguiar <michaelgerson@gmail.com>
2020-06-03 08:54:15 -0400

    Conectando com mongoDb

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,6 +1,8 @@
 package com.nelioalves.workshopmongo.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
@@ -10,10 +12,12 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Document(collection="user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     @EqualsAndHashCode.Include
     private String id;
     private String name;
